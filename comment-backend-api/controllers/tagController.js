4d8ab76de294606d3601a80e11c2881761bfcf49
sergey-kvitka <45879111+sergey-kvitka@users.noreply.github.com>
2025-04-21 13:25:45 +0400

    added tag controller

---
+++ comment-backend-api/controllers/tagController.js
@@ -0,0 +1,13 @@
+const Tag = require('../models/tagModel');
+require('dotenv').config();
+
+exports.all = async (req, res) => {
+    let tags;
+    try {
+        tags = await Tag.findByUser(req.user.id);
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+    if (!tags.length) return res.status(204);
+    res.status(200).json({ tags: tags });
+};
