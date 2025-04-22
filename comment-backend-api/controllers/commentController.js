e884be0adb2eeb391411fa9e5be04efbd75ee88e
sergey-kvitka <45879111+sergey-kvitka@users.noreply.github.com>
2025-04-22 15:49:18 +0400

    added getByFilters to comment controller

--- comment-backend-api/controllers/commentController.js
+++ comment-backend-api/controllers/commentController.js
@@ -15,9 +15,20 @@ exports.all = async (req, res) => {
 exports.save = async (req, res) => {
     try {
         req.body.comment.userId = req.user.id;
-        const comment = await Comment.save(req.body.comment);
+        const comment = await Comment.save({ ...req.body.comment, userId: req.user.id });
         res.status(200).json({ comment: comment });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 }
+
+exports.getByFilters = async (req, res) => {
+    let comments;
+    try {
+        comments = await Comment.findByFilters({ ...req.body, userId: req.user.id });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+    if (!comments.length) return res.status(204);
+    res.status(200).json({ comments: comments });
+};
